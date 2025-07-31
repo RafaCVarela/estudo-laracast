3430ddb21a571a5c7a46e57fdbdee49ef6ba205f
Rafael C Varela <rafael.varela2220@gmail.com>
2025-07-31 15:17:11 -0300

    3º update do arquivo index;php - Funções lambda e um maior controle de filtragem

--- index.php
+++ index.php
@@ -12,6 +12,12 @@
             margin: 0;
             font-family: sans-serif;
         }
+        h1 {
+            display: grid;
+            place-content: center;
+            margin: 0;
+            font-family: sans-serif;
+        }
     </style>
 </head>
 
@@ -27,41 +33,52 @@
                     "name" => "Androides Sonham com Ovelhas Elétricas?",
                     "author" => "Philip K. Dick",
                     "releaseYear" => 1968,
-                    "purchaseUrl" => "http://exemple.com"
+                    "purchaseUrl" => "https://www.amazon.com.br/Blade-Runner-Androides-ovelhas-el%C3%A9tricas/dp/8576574403"
             ],
             [
                     "name" =>"Projeto Avé Maria",
                     "author" => "Andy Weir",
                     "releaseYear" => 2021,
-                    "purchaseUrl" => "http://exemple.com"
+                    "purchaseUrl" => "https://www.amazon.com.br/Project-Hail-Mary-Andy-Weir/dp/171363029X"
             ],
             [
                     "name" => "Perdido em Marte",
                     "author" => "Andy Weir",
                     "releaseYear" => 2011,
-                    "purchaseUrl" => "http://exemple.com"
+                    "purchaseUrl" => "https://www.amazon.com.br/Perdido-em-Marte-Andy-Weir/dp/8580414482"
             ]
         ];
 
-        function filterByAuthor ($books, $author) {
+        // Função gernérica criada para filtrar
+        // Parametros são um array e uma função lambda ou função anônima ($items e $fn)
+        function filter ($items, $fn) {
 
-            $filteredBooks = [];
+            // Array filtrada
+            $filteredItems = [];
 
-            foreach ($books as $book){
-                if ($book['author'] === $author){
-                    $filteredBooks[] = $book;
+            // Looping para array assiociativa aninhada numa array
+            foreach ($items as $item){
+                if ($fn($item)){
+                    $filteredItems[] = $item;
                 }
             }
 
-            return $filteredBooks;
+            // Retorna a array filtrada
+            return $filteredItems;
         }
+
+        // Variável que armazenara a array filtrada, perceba que ela já tem o item que queremos, os livros
+        $filteredBooks = array_filter($books, function ($book){
+            // Função lambda que nos permite controlar melhor qual item queremos filtrar
+            return $book['releaseYear'] >= 2000;
+        });
 
     ?>
 
     <ul>
-        <?php foreach(filterByAuthor($books, 'Philip K. Dick') as $book): ?>
+        <?php foreach($filteredBooks as $book): ?>
             <li>
-                <a href="<?= $book['purchaseUrl'] ?>">
+                <a href="<?= $book['purchaseUrl'] ?>" target="_blank">
                     <?php echo "{$book['name']} ({$book['releaseYear']})";?> - Escrito por <?= $book['author'];?>
                 </a>
             </li>
