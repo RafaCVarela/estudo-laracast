6c568443eab96a7bffdd47fc7cb66efc76ac38bd
Rafael C Varela <rafael.varela2220@gmail.com>
2025-08-02 00:50:40 -0300

    Update nos arquivos - PDO primeiros passos

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
 
+// Conectar ao meu banco de dados MySQl
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=1243;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root');
+
+$stament = $pdo->prepare("SELECT * FROM post ");
+$stament->execute();
+
+$posts = $stament->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post){
+    echo "<li>" . $post['title'] . "</li>";
+}
