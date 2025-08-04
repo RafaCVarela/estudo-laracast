3bb3550ee44961827e6f1fbb385ed7ecb97b886b
Rafael C Varela <48953925+RafaCVarela@users.noreply.github.com>
2025-08-04 00:25:39 -0300

    Update Database.php
    Update - Não existe necessidade para 'password' para database

--- Database.php
+++ Database.php
@@ -6,7 +6,7 @@ class Database {
     public $connection;
     public function __construct()
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=1243;charset=utf8mb4";
+        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
         $this->connection = new PDO($dsn);
     }
 
