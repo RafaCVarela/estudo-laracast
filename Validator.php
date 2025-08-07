b3b72fb06c593ebc69b18372e53994182b28f165
Rafael C Varela <rafael.varela2220@gmail.com>
2025-08-07 16:56:15 -0300

    Intro to Form Validation & Extract a Simple Validator Class

---
+++ Validator.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Validator
+{
+    public static function validateNoteBody($body, $minLen = 1, $maxLen = INF)
+    {
+        $body = trim($body);
+
+        return strlen($body) > $minLen && strlen($body) <= $maxLen;
+    }
+
+    public static function validateEmail ($email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+}
--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,28 @@
 <?php
 
+
+require "Validator.php";
+
+$config = require "config.php";
+$db = new Database($config['database']);
+
 $heading = 'Criar Nota';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $formData = $_POST;
+
+    $errors = [];
+
+    if (!Validator::validateNoteBody(($_POST['body']), 1, 100)) {
+        $errors['body'] = 'A nota deve ter entre 1 e 100 caracteres.';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes (body, user_id) VALUE (:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);    
+    }
+    
 }
 
 require "views/note-create.view.php";
